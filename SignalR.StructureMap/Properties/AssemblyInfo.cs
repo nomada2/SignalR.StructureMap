b5452ba0a5bf940295a7cc58d5bbeddeb3edf37b
Jeff Doolittle <jeff.doolittle@lotpath.com>
2012-02-10 08:42:53 -0800

    ported SignalR Ninject dependency resolver to use StructureMap.

---
+++ SignalR.StructureMap/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalR.Ninject")]
+[assembly: AssemblyDescription("A ninject dependency resolver implementation for SignalR.")]
+[assembly: AssemblyCompany("David Fowler, Damian Edwards")]
+[assembly: AssemblyCopyright("Copyright © David Fowler and Damian Edwards 2011")]
+
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignalR.StructureMap/StructureMapDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using StructureMap;
+using SignalR.Infrastructure;
+
+namespace SignalR.Ninject 
+{
+    public class StructureMapDependencyResolver : DefaultDependencyResolver
+    {
+        private readonly IContainer _container;
+
+        public StructureMapDependencyResolver(IContainer container) 
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            _container = container;
+        }
+
+        public override object GetService(Type serviceType) 
+        {
+            return _container.GetInstance(serviceType) ?? base.GetService(serviceType);
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType) 
+        {
+            return _container.GetAllInstances(serviceType).Concat(base.GetServices(serviceType));
+        }
+    }
+}
